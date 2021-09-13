60d66933eff1e493519275cef55305f86f7d5e60
João Neto Silva Mendes <joaoneto_silvamendes@hotmail.com>
2021-09-13 19:05:58 -0300

    Projeto básico para estudo (conta bancaria)

---
+++ ConsoleApp1/ContaBancaria.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class ContaBancaria
+    {
+        //atributos
+        public int Numero { get; private set; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; }
+
+        //contrutores
+        public ContaBancaria(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+        public ContaBancaria(int numero, string titular, double saldo) : this (numero, titular)
+        {
+            Saldo = saldo;
+        }
+        //metódos
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        //override
+        public override string ToString()
+        {
+            return "Conta "
+                + Numero
+                + ", Titular: "
+                + Titular
+                + ", Saldo: $"
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
